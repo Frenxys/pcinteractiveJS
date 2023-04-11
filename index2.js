989985d06feaa5b6115edc353a0fbcc7e4ab5126
Frenxys <eneazani2005@gmail.com>
2023-04-11 14:14:09 +0200

    finito

--- index2.js
+++ index2.js
@@ -5,20 +5,24 @@ document.getElementById("form").addEventListener("submit", function(event) {
   let conta=0;
   immagini=document.getElementById("immagini");
   function stampaImmagini(val1,val2){
-    for(let i=0; i<val1*val2; i++){
-        
-        let immagine=document.createElement('img');
-        immagine.src="icona1.jpg";
-        immagine.width="100";
-        immagine.id=conta;
-        conta++;
-        immagine.height="100";
-        immagine.onclick = function() {
-            cambia(this);
-            };
-          
-        immagini.appendChild(immagine);
-        
+
+    for(let i=0; i<val1; i++){
+        div=document.createElement('div');
+        for(let i=0; i<val2; i++){
+            let immagine=document.createElement('img');
+            immagine.src="icona1.jpg";
+            immagine.width="100";
+            immagine.id=conta;
+            conta++;
+            immagine.height="100";
+            immagine.onclick = function() {
+                cambia(this);
+                };
+            
+            div.appendChild(immagine);
+            
+        }
+        immagini.appendChild(div);
     }
     
   }
