6c956780a68fabc82718c52cf22decd2bbf05e6c
Frenxys <eneazani2005@gmail.com>
2023-04-11 19:47:06 +0200

    ultimato

--- index2.js
+++ index2.js
@@ -5,12 +5,14 @@ document.getElementById("form").addEventListener("submit", function(event) {
   let conta=0;
   immagini=document.getElementById("immagini");
   function stampaImmagini(val1,val2){
-
+    if(val1==0 || val2==0){
+        alert("non inserire 0");
+    }
     for(let i=0; i<val1; i++){
         div=document.createElement('div');
         for(let i=0; i<val2; i++){
             let immagine=document.createElement('img');
-            immagine.src="icona1.jpg";
+            immagine.src="icona3.png";
             immagine.width="100";
             immagine.id=conta;
             conta++;
@@ -29,11 +31,11 @@ document.getElementById("form").addEventListener("submit", function(event) {
 function cambia(immagine){
     console.log(immagine.className);
     
-    if(immagine.className==="icona2"){
-        immagine.src="icona1.jpg";
-        immagine.className="icona1";
+    if(immagine.className==="icona4"){
+        immagine.src="icona3.png";
+        immagine.className="icona3";
     }else{
-        immagine.src="icona2.jpg";
-        immagine.className="icona2";
+        immagine.src="icona4.png";
+        immagine.className="icona4";
     }
-  }
+}
